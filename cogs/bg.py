daa3d05a7daa73f5141fa1472839f67677337131
Moksej <58531286+TheMoksej@users.noreply.github.com>
2020-10-30 13:25:23 +0100

    test

--- cogs/bg.py
+++ cogs/bg.py
@@ -13,6 +13,7 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+# test
 import discord
 import time
 from discord.ext import commands, tasks
