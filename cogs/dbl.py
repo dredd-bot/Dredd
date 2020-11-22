d8848057b46e126f1dbee41f8c8fd88386a07f89
Moksej <58531286+TheMoksej@users.noreply.github.com>
2020-11-22 21:59:48 +0100

    update design

--- cogs/dbl.py
+++ cogs/dbl.py
@@ -17,6 +17,7 @@ import dbl
 import discord
 from discord.ext import commands, tasks
 import config
+import datetime
 
 import asyncio
 
@@ -35,16 +36,23 @@ class DiscordBotsOrgAPI(commands.Cog, name="DBL"):
     @commands.Cog.listener()
     async def on_dbl_test(self, data):
         c = await self.bot.fetch_channel(780066719645040651)
-        await c.send(f"A vote test has ran succesfully!\n{data}")
+        e = discord.Embed(title='Upvote received', 
+                          url="https://top.gg/bot/667117267405766696/vote",
+                          color=0x5E82AC)
+        e.set_author(name=author, icon_url=author.avatar_url)
+        e.description = f"**{author}** has test voted for me on {datetime.datetime.now().__format__('%c')}"
+        e.set_thumbnail(url="https://cdn.discordapp.com/attachments/638902095520464908/659611283443941376/upvote.png")
+        await c.send(f"A vote test has ran succesfully!", embed=e)
 
     @commands.Cog.listener()
     async def on_dbl_vote(self, data):
         channel = self.bot.get_channel(780066719645040651)
         user = await self.bot.fetch_user(int(data['user']))
-        e = discord.Embed(color=0x5E82AC, 
-                          title="Received Upvote!",
-                          description=f"New upvote received from **{user}**!")
-        e.set_author(icon_url=user.avatar_url, name=str(user))
+        e = discord.Embed(title='Upvote received', 
+                          url="https://top.gg/bot/667117267405766696/vote",
+                          color=0x5E82AC)
+        e.set_author(name=author, icon_url=author.avatar_url)
+        e.description = f"**{author}** has voted for me on {datetime.datetime.now().__format__('%c')}"
         e.set_thumbnail(url="https://cdn.discordapp.com/attachments/638902095520464908/659611283443941376/upvote.png")
         await channel.send(embed=e)
 
