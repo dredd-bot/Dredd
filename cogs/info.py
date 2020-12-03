0e749541316a7d33c2970ba047d200d22f544be3
Moksej <58531286+TheMoksej@users.noreply.github.com>
2020-12-03 23:00:28 +0100

    fix color

--- cogs/info.py
+++ cogs/info.py
@@ -636,7 +636,7 @@ __**Other Information:**__
                           entries=_all,
                           thumbnail=None,
                           per_page = 15,
-                          embed_color=ctx.bot.embed_color,
+                          embed_color=self.color['embed_color'],
                           show_entry_count=True,
                           author=ctx.author)
         await paginator.paginate()
