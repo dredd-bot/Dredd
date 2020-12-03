5773e6f78fb953dc10538bcf0fc8a4c545b52781
Moksej <58531286+TheMoksej@users.noreply.github.com>
2020-12-03 01:26:03 +0100

    testing

--- cogs/help.py
+++ cogs/help.py
@@ -34,7 +34,8 @@ class HelpCommand(commands.HelpCommand):
                 'brief': 'See cog/command help',
                 'usage': '[category / command]',
                 'cooldown': commands.Cooldown(1, 3, commands.BucketType.user),
-                'name': 'help'})
+                'name': 'help',
+		'aliases': ['h']})
         self.verify_checks = True
         self.color = color_picker('colors')
         
