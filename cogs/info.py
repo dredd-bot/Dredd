bcb410b829167f7f1df819a15dee02f980f0d36d
Moksej <58531286+TheMoksej@users.noreply.github.com>
2021-01-01 00:36:17 +0100

    change thumbnail

--- cogs/info.py
+++ cogs/info.py
@@ -99,7 +99,7 @@ class info(commands.Cog, name="Info"):
 
         mems = len(self.bot.users)
 
-        file = discord.File("avatars/dreddthumbnailxmas.png", filename="dreddthumbnailxmas.png")
+        file = discord.File("avatars/dreddthumbnail.png", filename="dreddthumbnailxmas.png")
         Moksej = self.bot.get_user(345457928972533773)
 
         embed = discord.Embed(color=self.color['embed_color'])
@@ -111,7 +111,7 @@ __**Other Information:**__
 **Created:** {default.date(self.bot.user.created_at)} ({default.timeago(datetime.utcnow() - self.bot.user.created_at)})\n**Total:**\nCommands: **{totcmd:,}**\nMembers: **{mems:,}**\nServers: **{len(self.bot.guilds):,}**\nChannels: {emotes.other_unlocked} **{text:,}** | {emotes.other_vcunlock} **{voice:,}**\n
 """
         embed.set_image(
-            url='attachment://dreddthumbnailxmas.png')     
+            url='avatars/dreddthumbnail.png')     
 
         await ctx.send(file=file, embed=embed)
     
