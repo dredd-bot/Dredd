1eb7c366facafcaa6e11c9b74871b73a1f927dc7
Moksej <58531286+TheMoksej@users.noreply.github.com>
2021-05-14 17:13:38 +0200

    just a test

--- bot.py
+++ bot.py
@@ -136,7 +136,7 @@ class Bot(commands.AutoShardedBot):
                 webhooks=False,  # webhook update
                 invites=False,  # invite create/delete
                 voice_states=True,  # voice state update
-                presences=True,  # member/user update for games/activities
+                presences=False,  # member/user update for games/activities
                 guild_messages=True,  # message create/update/delete
                 dm_messages=True,  # message create/update/delete
                 guild_reactions=True,  # reaction add/remove/clear
