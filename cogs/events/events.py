ba6ff59264d6537cbcadf4ded27a311f61a4efa3
Moksej <saved.dat@gmail.com>
2021-05-16 20:22:52 +0200

    omg I'm such a big nerd

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -80,7 +80,7 @@ class Events(commands.Cog):
         ctx = await self.bot.get_context(message)
 
         if ctx.guild and not ctx.valid:
-            if message.guild.me.mentioned_in(message) and ctx.guild.default_role.mention not in message.content:
+            if message.guild.me.mentioned_in(message) and ctx.guild.default_role not in message.content:
                 prefix = CM.get(self.bot, 'prefix', message.guild.id)
                 if not prefix:  # If bot was invited when it was offline, or if for some reason the data was lost
                     self.bot.dispatch('guild_join', message.guild)
