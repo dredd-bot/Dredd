15b0d94de75cf4ccc54b1558b492ec245a8c0078
Moksej <saved.dat@gmail.com>
2021-05-16 20:17:27 +0200

    hopefully this fixes it

--- cogs/events/events.py
+++ cogs/events/events.py
@@ -79,8 +79,8 @@ class Events(commands.Cog):
 
         ctx = await self.bot.get_context(message)
 
-        if ctx.guild:
-            if self.bot.user.mentioned_in(message) and not ctx.valid and not ctx.guild.default_role.mentioned_in(message):
+        if ctx.guild and not ctx.valid:
+            if self.bot.user.mentioned_in(message) and ctx.guild.default_role.mention in message.content:
                 prefix = CM.get(self.bot, 'prefix', message.guild.id)
                 if not prefix:  # If bot was invited when it was offline, or if for some reason the data was lost
                     self.bot.dispatch('guild_join', message.guild)
